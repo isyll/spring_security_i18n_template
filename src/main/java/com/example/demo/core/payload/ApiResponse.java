f0255d8c369a0beff48923e5c29fa7b43e6c6945
Ibrahima Sylla <isyll.net@gmail.com>
2025-02-08 21:54:10 +0000

    refactor(ApiResponse): rename status to ok and update status handling

--- src/main/java/com/example/demo/core/payload/ApiResponse.java
+++ src/main/java/com/example/demo/core/payload/ApiResponse.java
@@ -6,10 +6,9 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.example.demo.core.utils.DateTimeUtils;
 import com.example.demo.core.utils.RequestUtils;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -54,10 +53,9 @@ public class ApiResponse<T> {
 
 	private Map<String, String> errors = null;
 
-	private boolean status;
+	private boolean ok;
 
-	@JsonProperty("status_code")
-	private int statusCode;
+	private int status;
 
 	private ZonedDateTime timestamp;
 
@@ -66,14 +64,14 @@ public class ApiResponse<T> {
 	public ApiResponse(String message, T data, HttpStatus httpStatus, Map<String, String> errors) {
 		this.message = message;
 		this.data = data;
-		this.status = httpStatus.is2xxSuccessful();
-		this.statusCode = httpStatus.value();
+		this.status = httpStatus.value();
+		this.ok = !httpStatus.isError();
 		this.timestamp = DateTimeUtils.getCurrentTimestamp();
 		this.errors = errors;
 		this.path = RequestUtils.getCurrentPath();
 	}
 
 	public ResponseEntity<ApiResponse<T>> toReponseEntity() {
-		return ResponseEntity.status(this.statusCode).body(this);
+		return ResponseEntity.status(this.status).body(this);
 	}
 }
